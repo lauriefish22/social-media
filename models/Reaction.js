393e9b7b42a5b01f2b0da0ee435fc270af5503fa
Laurie Fish <iamlauriefish@gmail.com>
2023-05-19 16:10:45 -0700

    finished models

---
+++ models/Reaction.js
@@ -0,0 +1,26 @@
+const { Schema } = require('mongoose');
+
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            //!date info needed
+        }
+    }
+)
+//!json?
+module.exports = reactionSchema 
---
+++ routes/api/User-routes.js
@@ -0,0 +1,20 @@
+//!GET all users
+
+//!GET a single user by _id
+
+//!POST a new user
+
+//!```json
+
+
+//!PUT to update a user by _id
+
+//!DELETE a user by _id
+
+//!`/api/users/:userId/friends/:friendId`**
+
+//! `POST` to add a new friend to a user's friend list
+
+//! `DELETE` to remove a friend from a user's friend list
+
+
