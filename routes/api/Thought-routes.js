2d45aeb76527186d403f2af58574d30bb55056b8
Laurie Fish <iamlauriefish@gmail.com>
2023-05-20 19:02:00 -0700

    added api routes

---
+++ routes/api/Thought-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/Thought-controller.js');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+module.exports = router;
---
+++ routes/api/User-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+
+} = require('../../controllers/User-controller.js');
+
+router.route('/').get(getUsers).post(createUser);
+
+router
+    .route('/:userId')
+get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
+
